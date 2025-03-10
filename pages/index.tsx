1e82b1930047a886f45b66f853aad6efef51e09a
laurasfelix <laurasfelix@hotmail.com.br>
2025-03-10 00:31:21 -0700

    better mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -165,7 +165,7 @@ export default function Home() {
           
 
           return (
-            <div key={index} ref={ref} className="w-screen flex bg-cover items-center p-2" style={{backgroundImage: `url('${part.image_src}')`, "justifyContent": part.header ? "start": "center", "alignItems": part.icon_part ? "center" : undefined, backgroundSize: part.image ? "90%" : "cover",backgroundRepeat: "no-repeat", backgroundPosition:"center"}}>
+            <div key={index} ref={ref} className="w-screen flex bg-cover items-center p-2 bg-md-90" style={{backgroundImage: `url('${part.image_src}')`, "justifyContent": part.header ? "start": "center", "alignItems": part.icon_part ? "center" : undefined ,backgroundRepeat: "no-repeat", backgroundPosition:"center"}}>
               
               {/* title of the story*/}
               {part.header && 
